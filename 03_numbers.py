7be0da44e689dce3a23b4ed5f2780758b9cb2a69
sudogaan <sudoogaan@gmail.com>
2022-04-14 15:08:03 +0300

    update

---
+++ 03_numbers.py
@@ -0,0 +1,14 @@
+# types of numbers: int, float, complex
+a=496 # this is a perfect number
+print(type(a))
+print(a)
+
+e = 2.718281828
+print(type(e))
+print(e)
+
+z = 2 - 6.1j
+
+print(type(z))
+print(z.real)
+print(z.imag)
